de936fa2085fd8cb00c21c140431f9becf42e61a
MichaelCZhou <1205464428@qq.com>
2018-03-02 18:55:26 +0800

    add sample

---
+++ point_cloud/src/pcl_sample.cpp
@@ -0,0 +1,36 @@
+/*
+*File Name pcl_sample
+*Author         shukai
+*Date           Mon Apr  4 09:29:48 CDT 2016
+*Description    A simple pcl demo
+*/
+
+
+#include<ros/ros.h>
+#include<pcl/point_cloud.h>
+#include<pcl_ros/point_cloud.h>
+#include<pcl_conversions/pcl_conversions.h>
+#include<sensor_msgs/PointCloud2.h>
+
+
+int main(int argc,char** argv)
+{
+
+
+ros::init(argc,argv,"pcl_sample");
+ros::NodeHandle nh;
+ros::Publisher pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("pcl_output",1);
+sensor_msgs::PointCloud2 output;
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+cloud->width = 100;
+cloud->height= 1;
+cloud->points.resize(cloud->width*cloud->height);
+
+//Convert the cloud to ROS message
+pcl::toROSMsg(*cloud,output);
+
+
+pcl_pub.publish(output);
+ros::spinOnce();
+return 0;
+}
